1691136faed9ee93301ca46797cea1826089e0c4
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-22 04:15:27 +0000

    setting html webpack pluging

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    }, 
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: './src/index.html',
+    }),
+  ],
+   
+ };
