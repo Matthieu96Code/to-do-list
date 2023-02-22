9c322bebd1add0dcf1b1f45c069c2994572e98c8
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-02-22 05:54:29 +0000

    add list as array object in java script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+// Array of todo task
+
+const todoTasks = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'complete todo list project',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'learn code',
+    completed: false,
+    index: 2,
+  },
+];
